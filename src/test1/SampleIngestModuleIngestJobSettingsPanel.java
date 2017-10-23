16af54b191697d14c036425dcbf37625fb772006
AAA <AAA@AAA>
2017-10-23 20:52:43 +0700

    Test Commit 1

---
+++ src/test1/SampleIngestModuleIngestJobSettingsPanel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+import org.sleuthkit.autopsy.ingest.IngestModuleIngestJobSettings;
+import org.sleuthkit.autopsy.ingest.IngestModuleIngestJobSettingsPanel;
+
+/**
+ *
+ * @author AAA
+ */
+public class SampleIngestModuleIngestJobSettingsPanel extends IngestModuleIngestJobSettingsPanel {
+
+    public SampleIngestModuleIngestJobSettingsPanel(SampleModuleIngestJobSettings sampleModuleIngestJobSettings) {
+    }
+
+    @Override
+    public IngestModuleIngestJobSettings getSettings() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
